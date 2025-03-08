fd1cadfa78ca8256ae77c94449f59c013a702463
Alexandre Landeau <alandeau@MacBook-Pro-de-Alexandre.local>
2025-03-08 12:16:19 +0100

    added caching

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 import logging
+import json
 
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import redis
 
 import utils.logging
 from core.pydantic import ChatRequest, ChatResponse
@@ -27,8 +29,11 @@ def create_application() -> FastAPI:
     )
     return app
 
-
 app = create_application()
+
+
+# Connect to Redis
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 
 @app.get("/")
@@ -40,9 +45,20 @@ async def hello_world() -> dict:
 async def complete(request: ChatRequest) -> ChatResponse:
     try:
         query = request.messages[-1].content
-        logging.info(f"Received query: {query}")
-        answer = run_model(query)
-        logging.info(f"Returned answer: {answer}")
+        cached_item = redis_client.get(query)
+        cache_hit = False
+
+        if cached_item:
+            logging.info(f"Found cached item for query: {query}")
+            answer = json.loads(cached_item.decode("utf-8"))
+            cache_hit = True
+        else:
+            logging.info(f"Received query: {query}")
+            answer = run_model(query)
+            logging.info(f"Returned answer: {answer}")
+            # Store the item in Redis for future requests
+            redis_client.setex(query, 86400,json.dumps(answer))
+
         return {
             "response": {
                 "content": answer.get("content"),
@@ -51,6 +67,7 @@ async def complete(request: ChatRequest) -> ChatResponse:
             "metadata": {
                 "sources": answer.get("sources"),
                 "display_sources": True,
+                "cache_hit": cache_hit
             },
         }
     except Exception as e:
